997648dd946e9bb5d539d93c9aebc64929d1312c
StevenTon-Temple <81384065+StevenTon-Temple@users.noreply.github.com>
2021-04-05 10:05:02 -0400

    Update Oregon Trail Game.py

--- Oregon Trail Game.py
+++ Oregon Trail Game.py
@@ -1,2 +1,12 @@
+import random
+days = 0
+maxdays = 30
 def organtrail():
-
+    while True:
+        if choice == "yes":
+            days +=1
+            print(days)
+        if days >= maxdays:
+            break
+nameofch = input("What is your name? ")
+choice = input("Your choice? ")
