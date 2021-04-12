20272a5f03a8435668947830ed6c8978e86ba6df
StevenTon-Temple <81384065+StevenTon-Temple@users.noreply.github.com>
2021-04-12 10:37:55 -0400

    Update Oregon Trail Game.py

--- Oregon Trail Game.py
+++ Oregon Trail Game.py
@@ -1,28 +1,29 @@
 import random
-nameofch = input("What is your name? ")
+nameofMC = input("What is your name? ").capitalize()
 def turn():
     days = 0
     maxdays = 15
     foodatbeginning = 10
     food = foodatbeginning
     while True:
-        end_day = input("Do you want to end day Y/Yes or N/No ").capitalize()
+        end_day = input("Do you want to end day? Y/Yes or N/No. ").capitalize()
         if end_day == "Y" and end_day != "N":
             days += 1
             print("Today is day", days)
             food = food - 1
-            print("You have left of food", food)
+            print("You have", food, "food left")
         if food == 0:
-            print(nameofch, "has died")
+            print(nameofMC, "has died.")
             return False
         elif end_day == "N":
             days = days
             print("Today is day", days)
         if days >= maxdays:
+            print("Congratulations", nameofMC, "you have surived.")
             return False
            
 def objectivesurvival():
-    print("Hello", nameofch, "welcome to Objective: Survival")
+    print("Hello", nameofMC, "welcome to Objective: Survival")
     while True:
         if turn() == False:
             break
