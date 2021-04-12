59e09e1fc57e56091b565eed89f9ff28dc2983d0
StevenTon-Temple <81384065+StevenTon-Temple@users.noreply.github.com>
2021-04-12 10:28:10 -0400

    Objective: Survival
    Added food and fixed turn day, added some list for actions and events.

--- Oregon Trail Game.py
+++ Oregon Trail Game.py
@@ -1,22 +1,34 @@
 import random
-nameofch = input("What is your name? ") 
+nameofch = input("What is your name? ")
 def turn():
     days = 0
     maxdays = 15
+    foodatbeginning = 10
+    food = foodatbeginning
     while True:
         end_day = input("Do you want to end day Y/Yes or N/No ").capitalize()
         if end_day == "Y" and end_day != "N":
             days += 1
             print("Today is day", days)
+            food = food - 1
+            print("You have left of food", food)
+        if food == 0:
+            print(nameofch, "has died")
+            return False
         elif end_day == "N":
             days = days
             print("Today is day", days)
         if days >= maxdays:
-            break
+            return False
+           
 def objectivesurvival():
     print("Hello", nameofch, "welcome to Objective: Survival")
+    while True:
+        if turn() == False:
+            break
     action1 = []
     action2 = []
 
 objectivesurvival()
-turn()
+#turn()
+#food()
